02020d59a81586612271e486356c0e8e1bf2ea4c
kystrader01-hub <kystrader01@gmail.com>
2026-07-27 17:44:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 import os
-import requests
 
 TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-data = {
-    "chat_id": CHAT_ID,
-    "text": "🤖 Test KysFx Bot"
-}
-
-r = requests.post(url, data=data)
-
-print("Status:", r.status_code)
-print("Response:", r.text)
+print("TOKEN:", TOKEN)
