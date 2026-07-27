bbfb101c6aa62685b78008b4a715e6de38afdc10
kystrader01-hub <kystrader01@gmail.com>
2026-07-27 17:50:47 +0800

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import os
+import requests
 
 TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-print("TOKEN:", TOKEN)
+url = f"https://api.telegram.org/bot{TOKEN}/getMe"
+
+r = requests.get(url)
+
+print("Status:", r.status_code)
+print("Response:", r.text)
