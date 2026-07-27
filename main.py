92418c53b2eb36f3ac04df1249165aabc198fa45
kystrader01-hub <kystrader01@gmail.com>
2026-07-27 16:50:07 +0800

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from telegram import Bot
 import os
+import requests
 
 TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-if TOKEN:
-    print("Bot siap dijalankan")
-else:
-    print("BOT_TOKEN belum diatur")
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+data = {
+    "chat_id": CHAT_ID,
+    "text": "🤖 KysFx Bot berhasil online!\n\nSelanjutnya saya akan mulai mengirim berita XAU/USD."
+}
+
+requests.post(url, data=data)
+
+print("Pesan berhasil dikirim.")
