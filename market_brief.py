361bf11f253dff274cabaf8bd2fe51bf445519f6
kystrader01-hub <kystrader01@gmail.com>
2026-07-28 10:17:13 +0800

    Update market_brief.py

--- market_brief.py
+++ market_brief.py
@@ -1,34 +1,63 @@
 from datetime import datetime
+from news import get_news
+
 
 def get_market_brief():
 
-    now = datetime.now()
+    berita = get_news()
 
-    jam = now.hour
+    top_news = berita[:3]
+
+    daftar = ""
+
+    if len(top_news) == 0:
+        daftar = "• Belum ada berita penting.\n"
+    else:
+        for item in top_news:
+            daftar += f"• {item['title']}\n"
+
+    jam = datetime.now().hour
 
     if 8 <= jam < 15:
-        sesi = "🌏 ASIA"
-
+        sesi = "🌏 ASIA MARKET OPEN"
     elif 15 <= jam < 20:
-        sesi = "🇬🇧 LONDON"
-
+        sesi = "🇬🇧 LONDON MARKET OPEN"
     else:
-        sesi = "🇺🇸 NEW YORK"
+        sesi = "🇺🇸 NEW YORK MARKET OPEN"
 
-    return f"""📈 MARKET OPEN BRIEF
+    pesan = f"""📈 {sesi}
 
-{sesi}
+━━━━━━━━━━━━━━━━━━
 
-━━━━━━━━━━━━━━
+📊 Bias Hari Ini
+🟡 Menunggu Konfirmasi
 
-📊 Bias
-🟡 Menunggu konfirmasi
+━━━━━━━━━━━━━━━━━━
 
-⚠️ Fokus
+📰 3 Berita Teratas
+
+{daftar}
+
+━━━━━━━━━━━━━━━━━━
+
+⚠️ Yang Perlu Diperhatikan
 
 • Perhatikan kekuatan USD
 • Pantau berita berdampak tinggi
 • Tunggu konfirmasi sebelum entry
 
-🎯 Disiplin trading lebih penting daripada banyak entry.
+━━━━━━━━━━━━━━━━━━
+
+🎯 Rencana Trading
+
+✓ Ikuti trend utama
+✓ Jangan FOMO
+✓ Gunakan manajemen risiko
+✓ Risk maksimal sesuai trading plan
+
+━━━━━━━━━━━━━━━━━━
+
+🤖 KysFx News Bot
 """
+
+    return pesan
